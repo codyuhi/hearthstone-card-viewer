fd39a3f0bf879f60c94d8cddb569ae89f75164b3
Cody Uhi <codylkuhi@gmail.com>
2021-12-20 11:36:51 -0700

    Create app Skeleton

---
+++ app/src/components/Card.vue
@@ -0,0 +1,39 @@
+<template>
+  <tr class="table-row">
+    <td colspan="2"></td>
+    <td>{{ card.name }}</td>
+    <td>{{ card.type ?? "N/A" }}</td>
+    <td>{{ card.rarity ?? "N/A" }}</td>
+    <td>{{ card.set ?? "N/A" }}</td>
+    <td>{{ card.class ?? "N/A" }}</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    card: Object,
+  },
+  data() {
+    return {
+      cardType: undefined,
+    };
+  },
+  mounted() {
+    switch (this.card.cardTypeId) {
+      case 4:
+        this.cardType = "Minion";
+        break;
+    }
+  },
+};
+</script>
+
+<style scoped>
+.table-row > td {
+  border: 1px solid #148eff;
+  padding: 15px;
+  margin: 0;
+}
+</style>
